8071d4c2de9d1d38b35f3d7e779e97d1980deeac
3bru <16ebruarslan@gmail.com>
2020-05-08 01:31:00 +0300

    change color

---
+++ build/precache-manifest.b93c8b09c151d418a6e7ef8452f9bffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2350151d6a18a9275d9089bb6382463",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11fe331b9d3cf99ad8a7",
+    "url": "/static/css/2.da564de3.chunk.css"
+  },
+  {
+    "revision": "11fe331b9d3cf99ad8a7",
+    "url": "/static/js/2.6ccf9b2d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.6ccf9b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0567fd0792e571d0cd4",
+    "url": "/static/js/main.7ee3ca59.chunk.js"
+  },
+  {
+    "revision": "f1d2aa6ddd291c675eaf",
+    "url": "/static/js/runtime-main.edc58dba.js"
+  }
+]);
